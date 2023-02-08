3b4e2bfd28b33420cc5a99aa337b7fba75802f0b
joshwayman <nowayman121@gmail.com>
2023-02-09 10:15:56 +1100

    Made the nav a bit better

--- js/navigation.js
+++ js/navigation.js
@@ -126,6 +126,27 @@
 	const menuGroupLvl1 = megaMenu.querySelector('[data-menu-level="1"]');
 	const menuGroupLvl2 = megaMenu.querySelector('[data-menu-level="2"]');
 
+	const allMenuItems = megaMenu.querySelectorAll('.menu-item');
+	const parentIds = [];
+
+	// Get all the parent Ids
+	allMenuItems.forEach( item => {
+		if(!item.dataset.parentId) return false;
+
+		parentIds.push(item.dataset.parentId);
+	});
+
+	// Loop through menu items and add a class to parents with no children to hide the arrow
+	allMenuItems.forEach( item => {
+		const groupId = item.dataset.groupId;
+		if(parentIds.includes(groupId)) return false;
+
+		item.classList.add('no-children');
+	})
+
+	console.log(parentIds);
+
+
 	megaMenuGroups.forEach (  menuGroup => {
 		const dataset = menuGroup.dataset;
 
@@ -137,7 +158,6 @@
 
 		// If no items return
 		if(!items) return false;
-
 
 
 		items.forEach( item => {
@@ -243,193 +263,3 @@
 	});
 
 } () );
-
-
-
-/*
- ( function() {
-	const siteNavigation = document.getElementById( 'nav' );
-	const button = document.querySelector( 'nav button' );
-	const page = document.getElementById('page');
-	const body = document.querySelector('body');
-
-	// Return early if the navigation don't exist.
-	if ( ! siteNavigation ) { return; }
-
-	// Return early if the buttons don't exist.
-	if ( 'undefined' === typeof button || button === null ) { return; }
-
-	const toggleMenu = ( direction = null ) => {
-		const span = button.querySelector('span');
-
-		if(direction === 'close') {
-			siteNavigation.classList.add('closed');
-			page.setAttribute( 'aria-expanded', 'false' );
-			span.innerText = 'menu';
-
-			return;
-		}
-
-		if(direction === 'open') {
-			siteNavigation.classList.remove('closed');
-			page.setAttribute( 'aria-expanded', 'true' ); 
-			body.setAttribute( 'aria-expanded', 'true' );
-			span.innerText = 'chevron_left';
-
-			return;
-		}
-
-		// Else just toggle it.
-		siteNavigation.classList.toggle('closed');
-		
-		span.innerText === 'menu' ? page.setAttribute( 'aria-expanded', 'true' ) : page.setAttribute( 'aria-expanded', 'false' );
-		span.innerText === 'menu' ? body.setAttribute( 'aria-expanded', 'true' ) : body.setAttribute( 'aria-expanded', 'false' );
-		span.innerText = span.innerText === 'menu' ? 'chevron_left' : 'menu';
-
-		return;
-	}
-
-	// Toggle the .toggled class and the aria-expanded value each time the button is clicked.
-	button.addEventListener( 'click', function() {
-		toggleMenu();
-
-		return true;
-	} );
-
-	const menu_items = document.querySelectorAll('nav a');
-	
-	menu_items.forEach( item => {
-		item.addEventListener('click', () => {
-
-			body.setAttribute( 'aria-expanded', 'false' );
-			toggleMenu('close');
-			
-		});
-	});
-
-}() );
-
-
-
-*/
-
-/**
- * Handle the scrolly boiz
- */
-/*
-( function() {
-	document.addEventListener('DOMContentLoaded', function () {
-		setTimeout(() => {
-
-			//console.log(page_els);
-
-			let pre = 0; // Store position of previous element offset;
-
-			const handleSectionPercentage = (position, element_id, top, bottom) => {
-				const height = bottom - top;
-				const adjusted_position = position - top;
-
-				const percent = Math.floor(adjusted_position / height * 100);
-				const menu_item = document.querySelector('nav li[data-menu-for="'+element_id+'"]');
-
-				//console.log(element_id, menu_item);
-
-				menu_item.dataset.sectionScrollPercentage = percent;
-			}
-
-			const scrollPercentElement = document.querySelector('#scrollPercentage');
-
-
-			const debugScrolling = (position, id, top, bottom) => {
-				const scrollPosition = document.querySelector('#scrollPosition');
-				const sectionActive = document.querySelector('#sectionActive');
-				const sectionTop = document.querySelector('#sectionTop');
-				const sectionEnd = document.querySelector('#sectionEnd');
-
-				scrollPosition.innerText = position;
-				sectionActive.innerText = id;
-				sectionTop.innerText = top;
-				sectionEnd.innerText = bottom;
-
-				const scrollDebugEnd = document.querySelector('#scrollDebugEnd');
-				const scrollDebugStart = document.querySelector('#scrollDebugStart');
-
-				scrollDebugStart.setAttribute('style', 'top: '+top+'px');
-				scrollDebugEnd.setAttribute('style', 'top: '+bottom+'px');
-				
-
-				return;
-			}
-
-			// Check offset on scroll
-			document.addEventListener('scroll', function ( event ) {
-				const actualPosition = window.scrollY;
-				const position = actualPosition + (63); // 3.5rem is the padding the section has from the top.
-
-				// Loop through page_els to check offest against position
-				page_els.forEach( obj => {
-
-					const el = obj.el;
-					const el_offset = obj.top;
-					//const previous_el = obj.previous;
-					const next_el = obj.next;
-					let el_offset_bottom = obj.bot;
-
-					// Check if the menu item is an inner guy.
-					if(obj?.tag === 'DIV' ) {
-						// Check if there is a next element.
-						if( next_el.top !== undefined ) { 
-							// If there is a next element set the top of that as the bottom of the el.
-							el_offset_bottom = next_el.top; 
-						}
-					}
-					
-					const target = document.querySelector('nav li[data-menu-for="#'+el.id+'"]');
-					const section = document.querySelector('#'+el.id);
-
-					
-
-
-					if( (position > el_offset) && (position < el_offset_bottom) ) {
-						target.classList.add('active');
-						section.classList.add('active');
-
-						//debugScrolling(position, el.id, el_offset, el_offset_bottom);
-
-						if(obj?.tag === 'SECTION' ) {
-							//handleSectionPercentage(position, obj.id, el_offset, el_offset_bottom);
-						}
-
-						//if( obj.id === '#karen' ) { console.warn(target, position, el_offset, el_offset_bottom ); }
-					} else {
-						target.classList.remove('active');
-						section.classList.remove('active');
-
-						//if( obj.id === '#karen' ) { console.log(target, position, el_offset, el_offset_bottom ); }
-					}
-					return;
-				});
-
-
-				let scrollTop = window.scrollY;
-				let docHeight = document.body.offsetHeight;
-				let winHeight = window.innerHeight;
-				let scrollPercent = scrollTop / (docHeight - winHeight);
-				let scrollPercentRounded = scrollPercent * 100;
-
-				//console.log(scrollPercentRounded);
-
-				if(!scrollPercentElement) return;
-
-				scrollPercentElement.style = '--scroll-percent: '+scrollPercentRounded+'%;';
-
-			}, {
-				passive: true
-			});
-
-		}, 200);
-	});
-
-}() );
-
-*/
