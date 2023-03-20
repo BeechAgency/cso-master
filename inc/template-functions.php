7b84a3ca62aac2e3320b7b15e35d6846a8c11782
joshwayman <nowayman121@gmail.com>
2023-03-20 12:42:18 +1100

    Updated settings and css tweaks

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -48,6 +48,7 @@ if( function_exists('acf_add_options_page') ) {
 		'redirect'		=> false
 	));
 	
+	// TODO: Remove this doggo.
 	acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Header Settings',
 		'menu_title'	=> 'Header',
