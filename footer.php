c462341a41493aa7f238b14e18acad7d077cf3ba
joshwayman <nowayman121@gmail.com>
2024-08-23 16:22:33 +1000

    Fixed a bug on the search menu background

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
  */
 
  	$search_background_image = get_field('search_background_image', 'option');
-    $search_background_style = "background: linear-gradient(0deg, var(--primary-dark) 0%, rgba(0,0,0,0.4) 100%), url($search_background_image); background-position: top center;";
+    $search_background_style = "background: linear-gradient(0deg, var(--primary-dark) 0%, rgba(0,0,0,0.4) 100%), url($search_background_image); background-position: top center; background-size: cover;";
 ?>
 
 	<?php get_template_part( 'template-parts/navs/nav', 'footer' ); ?>
