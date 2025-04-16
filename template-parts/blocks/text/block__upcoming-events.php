ad462fd2905ec57013f727bff1a691ecd986564d
joshwayman <nowayman121@gmail.com>
2025-04-16 11:24:07 +1000

    Fixed a bug in the events list

--- template-parts/blocks/text/block__upcoming-events.php
+++ template-parts/blocks/text/block__upcoming-events.php
@@ -19,6 +19,21 @@
     $upcoming_events = get_field('event_list', 'option');
 
     usort($upcoming_events, 'sortByDate');
+
+    $filtered_events = [];
+
+    $threshold_date = new DateTime();
+    $threshold_date->modify('-5 days'); // 5 days ago
+
+    foreach($upcoming_events as $event):
+        $event_date = DateTime::createFromFormat('d/m/Y', $event['event_date']);
+
+        // If the date is valid and not older than 5 days, keep it
+        if ($event_date && $event_date >= $threshold_date) {
+            $filtered_events[] = $event;
+        }
+    endforeach;
+
 ?>
 <div class="xy-col text-wrapper" data-xy-col="12" data-xy-start="auto">
     <div class="heading-group has-primary-color">
@@ -30,16 +45,17 @@
     <div class="xy-col events-image-col"  data-xy-col="xl-5 lg-5 md-12" data-xy-start="auto" >
         <div class="events-image-wrap">
             <?php 
-            if(!empty($upcoming_events)):
+            if(!empty($filtered_events)):
                 $event_index = 0;
-                foreach($upcoming_events as $event):
+                foreach($filtered_events as $event):
                     $event_image = $event['image'];
                     $event_title = $event['title'];
                     $classes = 'events-image event-image-with-index'; 
-                    $event_index++;
+                    
                     if(!empty($event_image)) {
                         echo wp_get_attachment_image($event_image, 'full', 0, array('title'=> $event_title, 'class'=> $classes, 'data-event-index' => $event_index, 'style' => '--_event-index: '. $event_index) );
                     }
+                    $event_index++;
                 endforeach;
             endif;
             ?>
@@ -49,9 +65,9 @@
     <div class="xy-col" data-xy-col='xl-6 lg-6 md-12' data-xy-start='xl-7 lg-7 md-auto'>
         <div class="events-list">
             <?php
-             if(!empty($upcoming_events)):
+             if(!empty($filtered_events)):
                 $event_index = 0;
-                foreach($upcoming_events as $event):
+                foreach($filtered_events as $event):
                     $title = $event['title'];
                     $desecription = $event['description'];
                     $link = $event['link'];
@@ -82,7 +98,11 @@
                     <?php
                     $event_index++;
                 endforeach;
-             endif; ?>
+            else :
+                // If no events are found, display a message
+                echo '<p>No upcoming events.</p>';
+
+            endif; ?>
         </div>
     </div>
 </div>
